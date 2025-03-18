b10057e836413fe9b6d1b06463a6128b170a1d9e
monick.s.g2 <monick.s.g2@gmail.com>
2025-03-18 02:09:39 +0000

    Semana 3

--- test/dart_test.dart
+++ test/dart_test.dart
@@ -23,11 +23,27 @@ int factorial(int n) {
 }
 
 bool isPrime(int num) {
-  return false;
+  if (num <= 1) {
+    return false; 
+  }
+  for (int i = 2; i * i <= num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+ return true;
 }
 
 int sumOfDigits(int num) {
-  return num;
+  if (num < 0) {
+    throw ArgumentError('O número não deve ser negativo.');
+  }
+  int sum = 0;
+  while (num > 0) {
+    sum += num % 10; 
+    num ~/= 10;      
+  }
+return sum;
 }
 
 void main() {
